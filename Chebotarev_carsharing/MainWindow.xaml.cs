9d82fe417573e29d3c83413f85ebde2ef39eb94e
settamancer <dmitriy.chebotarev@mail.ru>
2026-04-27 14:39:18 +0300

    captcha added

--- Chebotarev_carsharing/MainWindow.xaml.cs
+++ Chebotarev_carsharing/MainWindow.xaml.cs
@@ -20,9 +20,11 @@ namespace Chebotarev_carsharing
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Image firstButton;
         public MainWindow()
         {
             InitializeComponent();
+            LoadPuzzle();
         }
         private void txtUsername_GotFocus(object sender, RoutedEventArgs e)
         {
@@ -48,7 +50,7 @@ namespace Chebotarev_carsharing
 
         private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            if (txtPassword.SecurePassword.Length > 0 )
+            if (txtPassword.SecurePassword.Length > 0)
             {
                 lblPasswordHint.Visibility = Visibility.Collapsed;
             }
@@ -56,6 +58,58 @@ namespace Chebotarev_carsharing
             {
                 lblPasswordHint.Visibility = Visibility.Visible;
             }
+        }
+
+        private void LoadPuzzle()
+        {
+            var pices = Enumerable.Range(1, 4).OrderBy(x => new Random().Next()).ToList();
+            pices.ForEach(x =>
+            {
+                var img = new Image
+                {
+                    Source = new BitmapImage(new Uri($"Images/{x}.png", UriKind.Relative)),
+                    Tag = x,
+                    Stretch = Stretch.Fill
+                };
+                img.MouseLeftButtonUp += Pices_Click;
+
+                PuzzleGrid.Children.Add(img);
+            });
+        }
+
+        private void Pices_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Image clicked)
+            {
+                if (firstButton == null)
+                {
+                    firstButton = clicked;
+                    firstButton.Opacity = 0.5;
+                    return;
+                }
+                if (clicked != firstButton)
+                {
+                    (firstButton.Source, clicked.Source) = (clicked.Source, firstButton.Source);
+                    (firstButton.Tag, clicked.Tag) = (clicked.Tag, firstButton.Tag);
+                }
+
+                firstButton.Opacity = 1;
+                firstButton = null;
+                CheckPuzzle();
+            }
+
+        }
+
+        private void CheckPuzzle() 
+        {
+            var expectedOrder = new int[] { 2, 1, 3, 4 };
+            if (PuzzleGrid.Children.OfType<Image>()
+                    .Select((img, i) => i + 1 == (int)img.Tag)
+                    .All(x => x))
+            {
+                MessageBox.Show("Капча решена!");
+            }       
+             
         }
     }
 
